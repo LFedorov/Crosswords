c42dc988225d5ee307850d51fed3fcb6191199e6
unknown <fedorov86@gmail.com>
2016-10-10 12:39:51 +0300

    No message

--- src/app/crosswords/services/crosswords.service.ts
+++ src/app/crosswords/services/crosswords.service.ts
@@ -26,10 +26,30 @@ export class CrosswordsService {
     }
 
     public getIssue(id: number) {
-        return this._remoteData.getIssue(id);
+        return new Promise((resolve, reject) => {
+            this._localData
+                .getIssue(id)
+                .then(issue => resolve(issue))
+                .catch(() => {
+                    this._remoteData
+                        .getIssue(id)
+                        .then(issue => resolve(issue));
+                })
+                .catch((error) => reject(error));
+        });
     }
 
     public getCrossword(id: string): Promise<Crossword> {
-        return this._remoteData.getCrossword(id);
+        return new Promise((resolve, reject) => {
+            this._localData
+                .getCrossword(id)
+                .then(crossword => resolve(crossword))
+                .catch(() => {
+                    this._remoteData
+                        .getCrossword(id)
+                        .then(crossword => resolve(crossword));
+                })
+                .catch((error) => reject(error));
+        });
     }
 }
